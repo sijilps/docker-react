9e348adbea174414ccc6e34ba3a2767ba61d1a40
Root <root@localhost.com>
2020-01-24 13:53:26 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hi There !.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://sijil.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sijil 
         </a>
       </header>
     </div>
